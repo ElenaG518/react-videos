a95f0574e5975d011cdad5e009b8fb1228f4cdc7
Elena Granados <patty518@yahoo.com>
2019-04-13 21:42:23 -0700

    Updates

---
+++ precache-manifest.b4522916564d9391aa61f5d4feb0180d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad827d23e73279ca75eb",
+    "url": "/react-videos/static/js/runtime~main.f892f45d.js"
+  },
+  {
+    "revision": "14f052696177e3077ef6",
+    "url": "/react-videos/static/js/main.52c0db14.chunk.js"
+  },
+  {
+    "revision": "6b7a8549d14891e00d91",
+    "url": "/react-videos/static/js/2.1e7b24b2.chunk.js"
+  },
+  {
+    "revision": "14f052696177e3077ef6",
+    "url": "/react-videos/static/css/main.dd422fda.chunk.css"
+  },
+  {
+    "revision": "f3a0e030adb93460a7a597fd3ebbcf4d",
+    "url": "/react-videos/index.html"
+  }
+];
